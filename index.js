f46de183c092fc6e8fae2ee6594fb5b739b27a5c
ZubairSidd <zubairsidd20@gmail.com>
2021-04-01 14:47:37 +0530

    feat: confirmation message

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const log = console.log
 
 var score = 0
 log('Hello and Welcome to Marvel Quiz')
+
+if (readline.keyInYN('Are you ready to play this game \n'))
+{
+  log('Here we go \n--------------------------')
+} else {
+  log('Adios mate')
+}
